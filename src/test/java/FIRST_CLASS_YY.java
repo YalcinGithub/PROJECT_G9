72e55a4bbda9de0ec27300ae195142f79836ea99
Yalcin YILDIZ <64442783+yyildizz@users.noreply.github.com>
2022-04-21 17:59:37 +0200

    added extras

--- src/test/java/FIRST_CLASS_YY.java
+++ src/test/java/FIRST_CLASS_YY.java
@@ -1,7 +1,18 @@
 public class FIRST_CLASS_YY {
     public static void main(String[] args) {
         System.out.println("first project");
-        
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
 
     }
 }
